06f708c27c20657c894bfdffb50c0ccbb0765b7f
herngyih <herngyih.gan@gmail.com>
2025-07-03 13:49:36 +0800

    Update installNessus.ps1

--- installNessus.ps1
+++ installNessus.ps1
@@ -7,8 +7,13 @@ $VMs = $VMs | Where-Object { $_.Trim() -ne "" }
 # Initialize result array
 $results = @()
 
+# Initialize counters
+$total = $VMs.Count
+$index = 0
+
 foreach ($vm in $VMs) {
-    Write-Host "Installing in $vm..."
+    $index++
+    Write-Host "$index out of $total - Copying installer to $vm..." -NoNewline
     $session = $null
     try {
         # Establish remote session
@@ -21,13 +26,14 @@ foreach ($vm in $VMs) {
 
         # Copy installer to remote machine
         Copy-Item -Path "E:\NessusAgent-10.9.0-x64.msi" -Destination "C:\NessusAgent\NessusAgent-10.9.0-x64.msi" -ToSession $session -Force
-
+        Write-Host "Installing..." -NoNewline
         # Run silent Nessus Agent install over session
         Invoke-Command -Session $session -ScriptBlock {
             Start-Process msiexec.exe -ArgumentList '/i "C:\NessusAgent\NessusAgent-10.9.0-x64.msi" NESSUS_GROUPS="Windows-Servers" NESSUS_SERVER="10.10.2.185:8834" NESSUS_KEY=5c1abba71c32ee4c84cb46cc171eed3f8c195a7d7267f333ffe2364545fd4b5b /qn' -Wait -NoNewWindow
         }
 
         # Log success
+        Write-Host "Done"
         $results += [PSCustomObject]@{
             Timestamp = (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
             VM        = $vm
@@ -35,6 +41,7 @@ foreach ($vm in $VMs) {
         }
     }
     catch {
+        Write-Host "Fail"
         # Log failure with error message
         $results += [PSCustomObject]@{
             Timestamp = (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
